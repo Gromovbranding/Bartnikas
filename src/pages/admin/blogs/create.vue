e00ecc7d91097223fa60ae9a7debd915b488f194
Ivan <efcolipt@gmail.com>
2023-04-24 16:48:15 +0300

    fix: admin panel create

--- src/pages/admin/blogs/create.vue
+++ src/pages/admin/blogs/create.vue
@@ -1,34 +1,50 @@
 <script lang="ts" setup>
+import { UploadUserFile } from "element-plus";
+
 const name = ref("Create Article");
 
 useHeadSafe({
   title: name.value,
 });
+
+const { fetchPost } = useApi();
+
+const handleCreate = async (body: any, images: UploadUserFile[]) => {
+  console.log(images);
+  await fetchPost("/blogs", {
+    ...body,
+    images: [],
+  });
+};
 </script>
 
 <template>
   <div>
     <ClientOnly>
       <AdminCardCreate
-        :form="{
-          title: {
+        :form="[
+          {
             value: '',
             label: 'Title',
             type: 'input',
+            prop: 'title',
           },
-          desc: {
+          {
             value: '',
             label: 'Description',
             type: 'textarea',
+            prop: 'desc',
           },
-          text: {
+          {
             value: '',
             label: 'Text',
             type: 'textarea',
+            prop: 'text',
           },
-        }"
+        ]"
         :name="name"
         back="blogs"
+        :cb-create="handleCreate"
       />
     </ClientOnly>
   </div>
--- src/pages/admin/news/create.vue
+++ src/pages/admin/news/create.vue
@@ -1,38 +1,54 @@
 <script lang="ts" setup>
+import { UploadUserFile } from "element-plus";
+
 const name = ref("Create Article");
 
 useHeadSafe({
   title: name.value,
 });
 
-const form = reactive({
-  title: {
+const { fetchUploadImages } = useApi();
+
+const form = reactive([
+  {
     value: "",
     label: "Title",
     type: "input",
+    prop: "title",
   },
-  desc: {
+  {
     value: "",
     label: "Description",
     type: "textarea",
+    prop: "desc",
   },
-  text: {
+  {
     value: "",
     label: "text",
     type: "textarea",
+    prop: "text",
   },
-  is_hot: {
-    value: false,
-    label: "Is Hot",
-    type: "checkbox",
-  },
-});
+]);
+
+const handleCreate = async (body: any, images: UploadUserFile[]) => {
+  const ids = await fetchUploadImages(images);
+  console.log(body, ids);
+  // await fetchPost("/news", {
+  //   ...body,
+  //   images: ids,
+  // });
+};
 </script>
 
 <template>
   <div>
     <ClientOnly>
-      <AdminCardCreate :form="form" :name="name" back="news" />
+      <AdminCardCreate
+        :form="form"
+        :name="name"
+        back="news"
+        :cb-create="handleCreate"
+      />
     </ClientOnly>
   </div>
 </template>
