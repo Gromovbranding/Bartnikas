e5a3d417189876bcbd17d7a50f9f46ce68e0f898
Ivan <efcolipt@gmail.com>
2023-05-10 17:22:30 +0300

    feat: styling throttle & format date

--- src/composables/useThrottle.ts
+++ src/composables/useThrottle.ts
@@ -1,12 +1,20 @@
-export function useThrottle(fn: Function, time: number) {
-  let throttled = false;
-  return function (...args) {
-    if (!throttled) {
+export const useThrottle = (fn: Function, wait = 300) => {
+  let inThrottle: boolean,
+    lastFn: ReturnType<typeof setTimeout>,
+    lastTime: number;
+  return function (this: any, ...args: any) {
+    if (!inThrottle) {
       fn.apply(this, args);
-      throttled = true;
-      setTimeout(() => {
-        throttled = false;
-      }, time);
+      lastTime = Date.now();
+      inThrottle = true;
+    } else {
+      clearTimeout(lastFn);
+      lastFn = setTimeout(() => {
+        if (Date.now() - lastTime >= wait) {
+          fn.apply(this, args);
+          lastTime = Date.now();
+        }
+      }, Math.max(wait - (Date.now() - lastTime), 0));
     }
   };
-}
+};
--- src/utils/useDateFormat.ts
+++ src/utils/useDateFormat.ts
@@ -13,3 +13,42 @@ export const useDateFormat = () => {
     makeDateCorrect,
   };
 };
+
+/**
+ * Format Date
+ * @param {string} formatStr - string format date, .
+ * @param {Date | string} date - date
+ * @returns {string} Returns formatted String Date
+ * @example
+ * const date2 = new Date(2020, 7, 9, 9, 9, 9, 99);
+ * formatDate("YYYY-DD-MM hh:mm:ss M", date2);
+ * @example
+ * const date = new Date(2020, 7, 22, 22, 22, 22, 222);
+ * formatDate("YYYY-DD-MM hh:mm:ss M", date);
+ */
+export const formatDate = (formatStr: string, date: Date | string) => {
+  const padLeft = (str: string | number, num = 2, fill = "0") =>
+    String(str).padStart(num, fill);
+
+  const d = new Date(date);
+
+  const time: {
+    [x: string]: string;
+  } = {
+    YY: padLeft(d.getFullYear()).substr(2, 4),
+    YYYY: padLeft(d.getFullYear()),
+    MM: padLeft(d.getMonth() + 1),
+    DD: padLeft(d.getDate()),
+    hh: padLeft(d.getHours()),
+    mm: padLeft(d.getMinutes()),
+    ss: padLeft(d.getSeconds()),
+    M: padLeft(d.getMilliseconds(), 3),
+  };
+
+  return formatStr.replace(
+    new RegExp(`${Object.keys(time).join("|")}`, "g"),
+    (subStr: string) => {
+      return time[subStr] || "";
+    }
+  );
+};
