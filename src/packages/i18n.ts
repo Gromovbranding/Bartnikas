a5e1f2b9ece36ebf0d7110b925db42d312b97f9e
killergot <disney.132435morozko@mail.ru>
2024-01-19 23:26:10 +0300

    fit: add fr and de language

--- src/packages/i18n.ts
+++ src/packages/i18n.ts
@@ -13,6 +13,7 @@ export default defineI18nConfig(() => ({
             titles: {
                 news: 'News',
                 projects: 'Projects',
+                project: 'Project',
                 concept: 'Concept',
                 home: 'Home',
                 contacts: 'Contacts',
@@ -133,6 +134,7 @@ export default defineI18nConfig(() => ({
             titles: {
                 news: 'Новости',
                 projects: 'Проекты',
+                project: 'Проект',
                 concept: 'Концепция',
                 home: 'Главная',
                 contacts: 'Контакты',
@@ -140,7 +142,7 @@ export default defineI18nConfig(() => ({
                 awards: 'Награды',
                 paymentDylivery: 'Оплата & Доставка',
                 termsConditions: 'Правила & Условия',
-                videoCollection: 'Коллекция Видео',
+                videoCollection: 'Коллекции Видео',
                 cart: 'Корзина',
                 checkout: 'Проверка',
                 article: 'Статья',
@@ -239,5 +241,235 @@ export default defineI18nConfig(() => ({
             },
             footer: 'Разработан компанией Gromov Branding'
         },
+        fr: {
+            name: {
+                first: 'Stanislav',
+                second: 'Bartnikas'
+            },
+            welcome: 'Bonjour',
+            titles: {
+                news: 'Actualités',
+                projects: 'Projets',
+                project: 'Project',
+                concept: 'Concept',
+                home: 'Accueil',
+                contacts: 'Contacts',
+                faq: 'FAQ',
+                awards: 'Récompenses',
+                paymentDylivery: 'Paiement & Livraison',
+                termsConditions: 'Conditions générales',
+                videoCollection: 'Collection vidéo',
+                cart: 'Panier',
+                checkout: 'Validation de commande',
+                article: 'Article',
+                blog: 'Blog',
+                media: 'Médias',
+                about: 'À propos de moi',
+                projectImage: 'Image du projet',
+                testimonials: 'Témoignages'
+            },
+            buttons: {
+                viewProject: 'Voir les projets',
+                back: 'Retour',
+                pay: 'Payer'
+            },
+            contacts: {
+                locationLive: 'Stanislav Bartnikas réside à Moscou, Russie.',
+                pressInquiry: 'Demande de presse',
+                gallery: 'Représentation en galerie',
+                formDescript: `Pour l'achat corporatif ou personnel de tirages, veuillez nous contacter via le formulaire`,
+                yourName: 'Votre nom',
+                yourEmail: 'Votre adresse e-mail',
+                comment: 'Commentaire',
+                btnSubmit: 'Confirmer',
+            },
+            projects: {
+                request: 'Demande',
+                moreProjects: 'Plus de projets',
+                more: 'Plus',
+                viewCollection: 'Voir toute la collection',
+                order: {
+                    preferSize: {
+                        headline: 'Taille préférée:',
+                        delivered: 'Toutes les photos sont disponibles en deux proportions principales : 4:3 et 6:4',
+                        individual: 'Tailles individuelles disponibles sur demande.'
+                    },
+                    allPhotosProportions: {
+                        headline: 'Édition limitée:',
+                        print: `Toutes les photos sont imprimées en édition limitée, généralement
+                        à quelques dizaines d'exemplaires, en fonction de l'œuvre et de sa taille.`
+                    },
+                    limitedEdition: {
+                        headline: 'Impression sur papier:',
+                        print: `Papier d'art satiné Canson Infinity Platine Fibre Rag, 100%
+                        coton`
+                    },
+                    dilivery: {
+                        headline: 'Livraison:',
+                        print: 'Gratuite dans le monde entier (DHL Express)',
+                    },
+                    bestColors: 'Meilleures couleurs d\'intérieur:'
+                },
+            },
+            media: {
+                latestNews: 'Dernières nouvelles',
+                exhibitions: 'Expositions',
+                publications: 'Publications',
+                mediaKit: 'Kit média',
+                presentation: 'Présentations',
+                previous: 'Précédent',
+                next: 'Suivant',
+                downloadPDF: 'Télécharger le PDF'
+            },
+            cart: {
+                subtotal: 'Sous-total'
+            },
+            form: {
+                quantity: 'Quantité:'
+            },
+            port: {
+                collaboration: 'Collaboration avec',
+                request: 'Demande'
+            },
+            orderForm: {
+                fillForm: `Pour demander une photo pleine d'énergie, veuillez remplir le formulaire
+                ci-dessous.`,
+                yourName: 'Votre nom',
+                yourEmail: 'Votre adresse e-mail',
+                request: 'Demande'
+            },
+            projectCollab: {
+                unableToDisplay: 'Impossible d\'afficher le fichier PDF. Veuillez le télécharger à la place.',
+                howItWorks: 'Comment ça marche'
+            },
+            sectionHeader: {
+                viewAll: 'Tout voir'
+            },
+            sectionInteriosOrder: {
+                inInterios: 'Dans l\'intérieur',
+                project: 'Projet :',
+                bestColorsOfInrerior: 'Meilleures couleurs d\'intérieur :',
+                request: 'Demande'
+            },
+            awards: {
+                additionalInfo: `Au cours de sa carrière, Stanislav a remporté plus de 200 récompenses différentes. Certaines d'entre elles sont répertoriées ci-dessous.`
+            },
+            footer: 'Développé par Gromov Branding'
+        },
+        de: {
+            name: {
+                first: 'Stanislaw',
+                second: 'Bartnikas'
+            },
+            welcome: 'Hallo',
+            titles: {
+                news: 'Nachrichten',
+                projects: 'Projekte',
+                project: 'Projekt',
+                concept: 'Konzept',
+                home: 'Startseite',
+                contacts: 'Kontakte',
+                faq: 'FAQ',
+                awards: 'Auszeichnungen',
+                paymentDylivery: 'Zahlung & Lieferung',
+                termsConditions: 'Geschäftsbedingungen',
+                videoCollection: 'Video-Sammlung',
+                cart: 'Warenkorb',
+                checkout: 'Kasse',
+                article: 'Artikel',
+                blog: 'Blog',
+                media: 'Medien',
+                about: 'Über mich',
+                projectImage: 'Projektbild',
+                testimonials: 'Empfehlungen'
+            },
+            buttons: {
+                viewProject: 'Projekte anzeigen',
+                back: 'Zurück',
+                pay: 'Bezahlen'
+            },
+            contacts: {
+                locationLive: 'Stanislaw Bartnikas lebt in Moskau, Russland.',
+                pressInquiry: 'Presseanfrage',
+                gallery: 'Vertretung in der Galerie',
+                formDescript: `Für den Firmen- oder persönlichen Kauf von Drucken kontaktieren Sie uns bitte über das Formular`,
+                yourName: 'Ihr Name',
+                yourEmail: 'Ihre E-Mail-Adresse',
+                comment: 'Kommentar',
+                btnSubmit: 'Bestätigen',
+            },
+            projects: {
+                request: 'Anfrage',
+                moreProjects: 'Mehr Projekte',
+                more: 'Mehr',
+                viewCollection: 'Gesamte Sammlung anzeigen',
+                order: {
+                    preferSize: {
+                        headline: 'Bevorzugte Größe:',
+                        delivered: 'Alle Fotos sind in zwei Hauptverhältnissen erhältlich: 4:3 und 6:4',
+                        individual: 'Individuelle Größen auf Anfrage erhältlich.'
+                    },
+                    allPhotosProportions: {
+                        headline: 'Limitierte Auflage:',
+                        print: `Alle Fotos werden in limitierter Auflage gedruckt, normalerweise
+                        in einigen Dutzend Exemplaren, abhängig von der Arbeit und ihrer Größe.`
+                    },
+                    limitedEdition: {
+                        headline: 'Papierdruck:',
+                        print: `Künstlerisches Seidenpapier Canson Infinity Platine Fibre Rag, 100%
+                        Baumwolle`
+                    },
+                    dilivery: {
+                        headline: 'Lieferung:',
+                        print: 'Weltweit kostenlos (DHL Express)',
+                    },
+                    bestColors: 'Beste Innenraumfarben:'
+                },
+            },
+            media: {
+                latestNews: 'Neueste Nachrichten',
+                exhibitions: 'Ausstellungen',
+                publications: 'Veröffentlichungen',
+                mediaKit: 'Medien-Kit',
+                presentation: 'Präsentationen',
+                previous: 'Vorherige',
+                next: 'Nächste',
+                downloadPDF: 'PDF herunterladen'
+            },
+            cart: {
+                subtotal: 'Zwischensumme'
+            },
+            form: {
+                quantity: 'Menge:'
+            },
+            port: {
+                collaboration: 'Zusammenarbeit mit',
+                request: 'Anfrage'
+            },
+            orderForm: {
+                fillForm: `Um ein energetisch aufgeladenes Foto anzufordern, füllen Sie bitte das Formular aus
+                unten.`,
+                yourName: 'Ihr Name',
+                yourEmail: 'Ihre E-Mail-Adresse',
+                request: 'Anfrage'
+            },
+            projectCollab: {
+                unableToDisplay: 'PDF-Datei kann nicht angezeigt werden. Bitte stattdessen herunterladen.',
+                howItWorks: 'Wie es funktioniert'
+            },
+            sectionHeader: {
+                viewAll: 'Alle anzeigen'
+            },
+            sectionInteriosOrder: {
+                inInterios: 'Im Inneren',
+                project: 'Projekt :',
+                bestColorsOfInrerior: 'Beste Farben des Innenraums :',
+                request: 'Anfrage'
+            },
+            awards: {
+                additionalInfo: `Im Laufe seiner Karriere hat Stanislav mehr als 200 verschiedene Auszeichnungen gewonnen. Einige davon sind unten aufgeführt.`
+            },
+            footer: 'Entwickelt von Gromov Branding'
+        }
     }
 }))
