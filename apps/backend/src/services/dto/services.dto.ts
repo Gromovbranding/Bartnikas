e743d37aaa62ae69734b6f504296497bc4689543
Ivan <efcolipt@gmail.com>
2024-05-19 18:56:31 +0300

    chore: add active

--- apps/backend/src/services/dto/services.dto.ts
+++ apps/backend/src/services/dto/services.dto.ts
@@ -1,17 +1,100 @@
 import { ApiProperty, IntersectionType } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsBoolean, ValidateNested } from 'class-validator';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
 import { CreateTranslateLanguageDto } from 'src/shared/language/dto/create-translate.dto';
 
 export class CreateTranslatePhotoportalDto extends IntersectionType(
   CreateTranslateLanguageDto,
 ) {
   @ApiProperty()
-  @IsBoolean()
-  is_active?: boolean = false;
+  intro_title: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  intro_subtitle: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  intro_action: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  peculiarities_title: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  peculiarities_list: string[];
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  influence_title: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  influence_quote_text: string[];
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  influence_quote_accent_text: string[];
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  influence_quote_author: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  for_what_title: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  for_what_list: string[];
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  popup_title: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  popup_subtitle_accent: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  popup_subtitle: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  popup_btn: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  agreement: string;
 }
 
 export class CreatePhotoportalDto {
+  @ApiProperty({ default: false })
+  @IsBoolean()
+  is_active = false;
+
   @ApiProperty({ type: () => [CreateTranslatePhotoportalDto] })
   @ValidateNested({ each: true })
   @Type(() => CreateTranslatePhotoportalDto)
@@ -22,11 +105,51 @@ export class CreateTranslatePlaceOfPowerDto extends IntersectionType(
   CreateTranslateLanguageDto,
 ) {
   @ApiProperty()
-  @IsBoolean()
-  is_active?: boolean = false;
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  ultra_exclusive_anchor: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  subtitle: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  quote_text: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  quote_author: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  transformation_title: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  list_title: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  list_items: string;
 }
 
 export class CreatePlaceOfPowerDto {
+  @ApiProperty({ default: false })
+  @IsBoolean()
+  is_active = false;
+
   @ApiProperty({ type: () => [CreateTranslatePlaceOfPowerDto] })
   @ValidateNested({ each: true })
   @Type(() => CreateTranslatePlaceOfPowerDto)
