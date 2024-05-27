9ae201266ddc3b7f8c4ba7de96256eee38fe8b7d
Ivan <efcolipt@gmail.com>
2024-05-28 00:45:46 +0300

    fix: services

--- apps/backend/src/services/photoportal.service.ts
+++ apps/backend/src/services/photoportal.service.ts
@@ -213,6 +213,9 @@ export class PhotoportalService {
         // without * 100 will equal 8.00 price
         amount: 888 * 100,
         currency: 'eur',
+        automatic_payment_methods: {
+          enabled: true,
+        },
       });
 
       await this.photoportalOrderRepository.save({
