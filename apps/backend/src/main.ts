6be3b6e3216b837ffa1cc531bc94fa41f4bf6bd2
Ivan <efcolipt@gmail.com>
2024-05-28 15:05:45 +0300

    fix: services

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,6 +6,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn', 'debug', 'verbose'],
+    rawBody: true,
   });
 
   app.enableCors({
