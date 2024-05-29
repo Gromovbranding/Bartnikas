808750780d49eb10f4624028c9b367cb657d36be
Tagir <sulaev.tagir@mail.ru>
2024-05-29 16:38:10 +0300

    chore: fix

--- apps/frontend/src/components/Admin/EditorInput.vue
+++ apps/frontend/src/components/Admin/EditorInput.vue
@@ -7,12 +7,17 @@ defineProps<{
 defineEmits<{
   (e: 'update:modelValue', data: string): void;
 }>()
+
+const tinymceInit = {
+  forced_root_block: false
+}
 </script>
 
 <template>
   <Editor
     api-key="dalm256umq1jx74e91oxwk9nbzue0o8ef5fmt4ny68o8lcm7"
     :model-value="modelValue"
+    :init="tinymceInit"
     style="width: 100%"
     @update:model-value="$emit('update:modelValue', $event)"
   />
