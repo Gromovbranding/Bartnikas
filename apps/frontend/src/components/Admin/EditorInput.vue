c9e0261b827a0e6c72ef49ccd354c7653edb56e6
Ivan <efcolipt@gmail.com>
2024-10-19 20:02:21 +0300

    fix: add insert urls

--- apps/frontend/src/components/Admin/EditorInput.vue
+++ apps/frontend/src/components/Admin/EditorInput.vue
@@ -9,12 +9,17 @@ defineEmits<{
 }>()
 
 const tinymceInit = {
-  plugins: 'paste',
-  paste_as_text: true,
-  relative_urls: false,
-  remove_script_host: true,
-  allow_script_urls: true
+  toolbar_mode: 'sliding',
+  plugins: [
+    // Core editing features
+    'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
+    // Your account includes a free trial of TinyMCE premium features
+    // Try the most popular premium features until Nov 2, 2024:
+    'checklist', 'mediaembed', 'casechange', 'export', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'editimage', 'advtemplate', 'ai', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown'
+  ],
+  toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat'
 }
+
 </script>
 
 <template>
